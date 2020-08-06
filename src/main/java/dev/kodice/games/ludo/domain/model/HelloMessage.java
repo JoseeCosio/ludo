b9c5d333a84329f307bab157306f7eca78ef2452
José Eduardo Cosío <jeduardo.cosio14@gmail.com>
2020-08-06 02:48:25 -0700

    no message

--- src/main/java/dev/kodice/games/ludo/domain/model/HelloMessage.java
+++ src/main/java/dev/kodice/games/ludo/domain/model/HelloMessage.java
@@ -1,21 +1,10 @@
 package dev.kodice.games.ludo.domain.model;
 
+import lombok.Data;
+
+@Data
 public class HelloMessage {
 
-	  private String name;
+	private String name;
 
-	  public HelloMessage() {
-	  }
-
-	  public HelloMessage(String name) {
-	    this.name = name;
-	  }
-
-	  public String getName() {
-	    return name;
-	  }
-
-	  public void setName(String name) {
-	    this.name = name;
-	  }
-	}
+}
